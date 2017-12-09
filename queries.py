e8e1b745febea51c05593b4b0beb3460b09800ac
Alexander Leighton <a.leighton@protonmail.com>
2017-12-09 17:41:07 +0800

    Updated queries.py to adhere to pep8

--- queries.py
+++ queries.py
@@ -2,6 +2,7 @@ import psycopg2
 
 DBNAME = 'news'
 
+
 def get_top_articles():
     QUERY = """SELECT articles.title, count(*) AS views
             FROM articles JOIN log
@@ -18,6 +19,8 @@ def get_top_articles():
         row = c.fetchone()
     db.close()
     print "\n"
+
+
 def get_top_authors():
     QUERY = """SELECT authors.name, SUM(article_views.views) AS views
             FROM authors JOIN article_views
@@ -33,10 +36,13 @@ def get_top_authors():
         row = c.fetchone()
     db.close()
     print "\n"
+
+
 def get_request_errors():
     QUERY = """SELECT percentages.time, percentages."% of bad requests"
             FROM (SELECT bad_requests.time,
-            CAST(bad_requests.bad_requests AS FLOAT)/total_requests.total_requests*100.0
+            CAST(bad_requests.bad_requests AS FLOAT)
+            /total_requests.total_requests*100.0
             AS "% of bad requests"
             FROM bad_requests JOIN total_requests
             ON bad_requests.time = total_requests.time) percentages
@@ -51,6 +57,8 @@ def get_request_errors():
         row = c.fetchone()
     db.close()
     print "\n"
+
+
 get_top_articles()
 get_top_authors()
 get_request_errors()
